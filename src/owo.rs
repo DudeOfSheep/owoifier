4ae859e33bb6302c634bc41b1db427ece276b67f
Mudshoes <kg082108@wgcloud.org>
2025-08-24 00:22:31 -0500

    Foundation of pattern matching and replacement

--- src/owo.rs
+++ src/owo.rs
@@ -1,5 +1,47 @@
 pub mod owo {
+    use std::{collections::HashMap, str::pattern::Pattern};
     trait Owoifier {
-        fn owoify(text: &str) -> &str;
+        fn replace_pass(text: &str) -> &str;
+        fn extension_pass(text: &str) -> &str;
+        fn insertion_pass(text: &str) -> &str;
+    }
+
+    struct Context<'a> {
+        pattern_map: HashMap<&'a str, &'a str>,
+        lookahead: i32,
+    }
+
+    impl<'a> Context<'a> {
+        fn new(pattern_map: HashMap<&'a str, &'a str>) -> Context<'a> {
+            let mut lookahead: i32 = 0;
+
+            for key in pattern_map.keys() {
+                let key_len = key.len() as i32;
+                lookahead = if key_len > lookahead {
+                    key_len
+                } else {
+                    lookahead
+                };
+            }
+
+            Context {
+                pattern_map,
+                lookahead,
+            }
+        }
+    }
+
+    pub fn basic_convert(text: &mut String) {
+        let buffer: String = text.drain(..).collect();
+
+        for item in buffer.chars() {
+            if item.is_alphabetic() {
+                match item {
+                    'r' => text.push('w'),
+                    'l' => text.push('w'),
+                    n => text.push(n),
+                }
+            }
+        }
     }
 }
