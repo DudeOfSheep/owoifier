f075930b39c05e397ec4d5310d48a884956decb6
Mudshoes <kg082108@wgcloud.org>
2025-08-24 12:58:28 -0500

    Removed lookahead field from Context, altered convert_string to take a immutable reference.

--- src/owo.rs
+++ src/owo.rs
@@ -5,34 +5,21 @@ pub mod owo {
     // Lookahed dictates how far the matching algorithim looks ahead before making a final decision.
     pub struct Context<'a> {
         pattern_map: HashMap<&'a str, &'a str>,
-        lookahead: i32,
     }
 
     impl<'a> Context<'a> {
         pub fn new(pattern_map: HashMap<&'a str, &'a str>) -> Context<'a> {
-            let mut lookahead: i32 = 0;
-
-            for key in pattern_map.keys() {
-                let key_len = key.len() as i32;
-
-                // Ugly
-                lookahead = if key_len > lookahead {
-                    key_len
-                } else {
-                    lookahead
-                };
-            }
-
-            Context {
-                pattern_map,
-                lookahead,
-            }
+            Context { pattern_map }
         }
 
-        pub fn convert_string(&self, text: &mut String) {
+        pub fn convert_string(&self, text: &String) -> String {
+            let mut buf = text.clone();
+
             for (k, r) in self.get_pattern_map().iter() {
-                *text = text.replace(k, r);
+                buf = buf.replace(k, r);
             }
+
+            buf
         }
 
         fn get_pattern_map(&self) -> &HashMap<&str, &str> {
