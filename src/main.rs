8463ea48c36da1c91b2d73a1ea706c088b09bd05
Mudshoes <kg082108@wgcloud.org>
2025-08-29 15:15:10 -0500

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod setup;
 
 use crate::owo::owo::Owoifier;
 use crate::setup::configuration::env_config::{Config, ConfigType};
+use std::thread::panicking;
 use std::{collections::HashMap, env, error::Error};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -16,23 +17,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let config: Config = Config::parse_args(&args)?;
-    let mut pattern_map: HashMap<&str, &str> = HashMap::new(); // Move this to file parse
-
     let mut buf = String::new();
 
     match config.get_format() {
-        ConfigType::BARE(n) => run(n),
-        ConfigType::FILE(n) => run(n), // Read the file, error checking, then convert
+        ConfigType::BARE(n) => run(n, &config),
+        ConfigType::FILE(n) => run(n, &config), // Read the file, error checking, then convert
     }
 
     Ok(())
 }
 
 fn run(text: &String, config: &Config) {
+    let mut pattern_map: HashMap<&str, &str> = HashMap::new();
     // Parse pattern file
-    match  config.{
-        
+    match config.get_intensity_pattern(&mut pattern_map) {
+        Ok(true) => (),
+        Ok(false) => panic!("Pattern file could not be found!"),
+        Err(_) => panic!("Pattern file could not be parsed!"),
     }
+
     let translator = Owoifier::new(pattern_map);
-    println!(translator.convert_string(text.clone()))
+
+    println!("{}", translator.convert_string(&text))
 }
